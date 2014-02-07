6fc581e6c6f6b3853a7a205ce11151483e5d285c
Wes Eldridge <weseldridge@gmail.com>
2014-02-07 09:25:49 -0600

    added examples of base64 and hex, and updated doc

--- README.md
+++ README.md
@@ -4,16 +4,18 @@ This class is very simple and is used for utility. Two public methos are avalaib
 encrypting and decrypting strings.
 
 
+## Storing in a database
+
+The initialisation vector and the encrypted string are both binary values. This means you will need to
+store them as such. In MySQL you can do this by making the column type `BINARY` or `VARBINARY`. If this
+is not an option, you can [encode](http://us3.php.net/manual/en/function.base64-encode.php) (`base64_encode`) and [decode](http://us3.php.net/manual/en/function.base64-decode.php) ('base64_decode) the iv and encrypted
+string going and coming from the db. [bin2hex](http://us1.php.net/bin2hex) is also an option.
+
 ## Todo
 
 * Add support for different Agorithm and Modes.
- 
- 
- 
- 
- 
- 
- 
+* Add support for base64 or hex vi and encrpted strings
+  
  
 I would like to thank Jack from stackoverflow
 http://stackoverflow.com/questions/10916284/how-to-encrypt-decrypt-data-in-php
--- example/index.php
+++ example/index.php
@@ -1,9 +1,5 @@
 <?php 
 	include '../src/BiCrypt.php'; 
-	$password = 'Password123';
-	$obj = new BiCrypt();
-	$encrypted_string = $obj->encrypt($password);
-	$decrypted_string = $obj->decrypt($encrypted_string['data'],$encrypted_string['iv'],$encrypted_string['key']);
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +16,13 @@
  		<div class="row">
  			<div class="col-sm-8 col-sm-offset-2">
  				<dl class="dl-horizontal">
+					<h3>Printed with bin</h3>
+					<?php
+					$password = 'Password123';
+					$obj = new BiCrypt();
+					$encrypted_string = $obj->encrypt($password);
+					$decrypted_string = $obj->decrypt($encrypted_string['data'],$encrypted_string['iv'],$encrypted_string['key']);
+					?>
 				  <dt>Password:</dt>
 				  <dd><?php echo $password; ?></dd>
 				  <dt>Key:</dt>
@@ -28,6 +31,56 @@
 				  <dd><?php echo $encrypted_string['iv']; ?></dd>
 				  <dt>Encrypted Passwrd:</dt>
 				  <dd><?php echo $encrypted_string['data']; ?></dd>
+				  <dt></dt>
+				  <dd></dd>
+				  <dt>Decrypted Password:</dt>
+				  <dd><?php echo $decrypted_string; ?></dd>
+				</dl>
+ 			</div>
+ 		</div>
+ 		<div class="row">
+ 			<div class="col-sm-8 col-sm-offset-2">
+				<h3>Printed using base64</h3>
+ 				<dl class="dl-horizontal">
+					<?php
+					$password = 'Password123';
+					$obj = new BiCrypt();
+					$encrypted_string = $obj->encrypt($password);
+					$decrypted_string = $obj->decrypt($encrypted_string['data'],$encrypted_string['iv'],$encrypted_string['key']);
+					?>
+				  <dt>Password:</dt>
+				  <dd><?php echo $password; ?></dd>
+				  <dt>Key:</dt>
+				  <dd><?php echo base64_encode($encrypted_string['key']); ?></dd>
+				  <dt>iv:</dt>
+				  <dd><?php echo base64_encode($encrypted_string['iv']); ?></dd>
+				  <dt>Encrypted Passwrd:</dt>
+				  <dd><?php echo base64_encode($encrypted_string['data']); ?></dd>
+				  <dt></dt>
+				  <dd></dd>
+				  <dt>Decrypted Password:</dt>
+				  <dd><?php echo $decrypted_string; ?></dd>
+				</dl>
+ 			</div>
+ 		</div>
+ 		<div class="row">
+ 			<div class="col-sm-8 col-sm-offset-2">
+ 				<dl class="dl-horizontal">
+					<h3>Printed using hex</h3>
+					<?php
+					$password = 'Password123';
+					$obj = new BiCrypt();
+					$encrypted_string = $obj->encrypt($password);
+					$decrypted_string = $obj->decrypt($encrypted_string['data'],$encrypted_string['iv'],$encrypted_string['key']);
+					?>
+				  <dt>Password:</dt>
+				  <dd><?php echo $password; ?></dd>
+				  <dt>Key:</dt>
+				  <dd><?php echo bin2hex($encrypted_string['key']); ?></dd>
+				  <dt>iv:</dt>
+				  <dd><?php echo bin2hex($encrypted_string['iv']); ?></dd>
+				  <dt>Encrypted Passwrd:</dt>
+				  <dd><?php echo bin2hex($encrypted_string['data']); ?></dd>
 				  <dt></dt>
 				  <dd></dd>
 				  <dt>Decrypted Password:</dt>
